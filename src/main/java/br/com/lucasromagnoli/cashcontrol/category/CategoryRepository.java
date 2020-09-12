21b1ace2ff826982edc320ac488f3a9ca3dbd05f
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-09-12 15:40:09 -0300

    Configurando a entidade Subcategory e implementando seu repository e service

--- src/main/java/br/com/lucasromagnoli/cashcontrol/category/CategoryRepository.java
+++ src/main/java/br/com/lucasromagnoli/cashcontrol/category/CategoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
--- src/main/java/br/com/lucasromagnoli/cashcontrol/subcategory/SubcategoryService.java
+++ src/main/java/br/com/lucasromagnoli/cashcontrol/subcategory/SubcategoryService.java
@@ -1,4 +1,19 @@
 package br.com.lucasromagnoli.cashcontrol.subcategory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class SubcategoryService {
+
+    @Autowired
+    private SubcategoryRepository subcategoryRepository;
+
+    public Subcategory save(Subcategory subcategory) {
+        return subcategoryRepository.save(subcategory);
+    }
+
+    public List<Subcategory> list() {
+        return subcategoryRepository.findAll();
+    }
 }
