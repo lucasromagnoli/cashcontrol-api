b7bdb3b27e215cbf4e2ed8570fb979a9cc5baa51
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-10-13 23:44:31 -0300

    Iniciando a implementacão das operacões das subcategorias

---
+++ src/main/java/br/com/lucasromagnoli/cashcontrol/subcategory/SubcategoryInputValidator.java
@@ -0,0 +1,56 @@
+package br.com.lucasromagnoli.cashcontrol.subcategory;
+
+import br.com.lucasromagnoli.cashcontrol.exception.CashControlRuntimeException;
+import br.com.lucasromagnoli.cashcontrol.validator.CheckRequired;
+import br.com.lucasromagnoli.cashcontrol.validator.PredicatesValidator;
+import br.com.lucasromagnoli.cashcontrol.validator.ValidatorOperation;
+import br.com.lucasromagnoli.cashcontrol.validator.ValidatorSupport;
+
+public class SubcategoryInputValidator {
+    private SubcategoryInputValidator() {}
+
+    private static ValidatorSupport<?> validateCommonsSaveAndUpdate(Subcategory subcategory,
+                                                                    ValidatorOperation validatorOperation) {
+        try {
+            return ValidatorSupport.fieldType(String.class)
+                    .target(subcategory)
+                    .operation(validatorOperation)
+                    .checkRequired(CheckRequired.SINGLE)
+                    .predicate(PredicatesValidator.stringLengthBetween(3, 50))
+                    .message("O nome precisa estrar entre 3 a 50 caracteres")
+                    .field("name")
+                    .validate()
+                    .field("description")
+                    .predicate(PredicatesValidator.stringLengthBetween(1, 200))
+                    .message("A descricão precisa estrar entre 1 a 200 caracteres")
+                    .validate()
+                    .field("category")
+                    .validate();
+
+        } catch (NoSuchFieldException e) {
+            // TODO: 10/11/20 - Inserir logger
+            e.printStackTrace();
+            throw new CashControlRuntimeException();
+        }
+    }
+
+    public static void validateSave(Subcategory subcategory) {
+        validateCommonsSaveAndUpdate(subcategory, ValidatorOperation.CREATE);
+    }
+
+    public static void validateUpdate(Subcategory subcategory) {
+        try {
+            validateCommonsSaveAndUpdate(subcategory, ValidatorOperation.UPDATE)
+                    .field("id")
+                    .validate();
+        } catch (NoSuchFieldException e) {
+            // TODO: 10/11/20 - Inserir logger
+            e.printStackTrace();
+            throw new CashControlRuntimeException();
+        }
+    }
+
+    public static void validateDelete(Subcategory subcategory) {
+        ValidatorSupport.requiredFields(subcategory, ValidatorOperation.DELETE);
+    }
+}
---
+++ src/main/java/br/com/lucasromagnoli/cashcontrol/subcategory/SubcategoryRestController.java
@@ -0,0 +1,78 @@
+package br.com.lucasromagnoli.cashcontrol.subcategory;
+
+import br.com.lucasromagnoli.cashcontrol.api.MessageTypeEnum;
+import br.com.lucasromagnoli.cashcontrol.api.TemplateMessage;
+import br.com.lucasromagnoli.cashcontrol.api.TemplateMessageSupport;
+import br.com.lucasromagnoli.cashcontrol.bootstrap.CashControlSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/subcategory")
+public class SubcategoryRestController {
+    @Autowired
+    private SubcategoryService subcategoryService;
+
+    @Autowired
+    private CashControlSupport cashControlSupport;
+
+    @GetMapping
+    public ResponseEntity<TemplateMessage> index(@PageableDefault(page = 0, size = 20) Pageable pageable) {
+        Page<Subcategory> subcategories = subcategoryService.findAll(pageable);
+        return TemplateMessageSupport.begin()
+                .message(cashControlSupport.getPropertie("cashcontrol.messages.operation.list.generic"))
+                .messageType(MessageTypeEnum.SUCCESS)
+                .payload(subcategories)
+                .build()
+                .toResponseEntity();
+    }
+
+    @PostMapping
+    public ResponseEntity<TemplateMessage> save(@RequestBody SubcategoryDto subcategoryDto) {
+        Subcategory subcategory = SubcategoryMapper.INSTANCE.toSave(subcategoryDto);
+        SubcategoryInputValidator.validateSave(subcategory);
+        subcategoryService.save(subcategory);
+
+        return TemplateMessageSupport.begin()
+                .message(cashControlSupport.getPropertie(
+                        "cashcontrol.messages.operation.save.success.detailed", subcategory.getName()))
+                .messageType(MessageTypeEnum.SUCCESS)
+                .payload(subcategory)
+                .build()
+                .toResponseEntity();
+    }
+
+    @PutMapping
+    public ResponseEntity<TemplateMessage> update(@RequestBody SubcategoryDto subcategoryDto) {
+        Subcategory subcategory = SubcategoryMapper.INSTANCE.toUpdate(subcategoryDto);
+        SubcategoryInputValidator.validateUpdate(subcategory);
+        subcategoryService.update(subcategory);
+
+        return TemplateMessageSupport.begin()
+                .message(cashControlSupport.getPropertie(
+                        "cashcontrol.messages.operation.update.success.detailed", subcategory.getName()))
+                .messageType(MessageTypeEnum.SUCCESS)
+                .payload(subcategory)
+                .build()
+                .toResponseEntity();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<TemplateMessage> delete(@RequestBody SubcategoryDto subcategoryDto) {
+        Subcategory subcategory = SubcategoryMapper.INSTANCE.toDelete(subcategoryDto);
+        SubcategoryInputValidator.validateDelete(subcategory);
+        subcategoryService.delete(subcategory);
+
+        return TemplateMessageSupport.begin()
+                .message(cashControlSupport.getPropertie(
+                        "cashcontrol.messages.operation.delete.success.detailed", subcategory.getId()))
+                .messageType(MessageTypeEnum.SUCCESS)
+                .payload(subcategory)
+                .build()
+                .toResponseEntity();
+    }
+}
