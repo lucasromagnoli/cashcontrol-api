9e7f7b62e4265b695bc0b4befa7c1734b37ce5df
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2021-01-05 21:00:49 -0300

    Implementando o service das movimentações

--- src/main/java/br/com/lucasromagnoli/cashcontrol/transaction/TransactionService.java
+++ src/main/java/br/com/lucasromagnoli/cashcontrol/transaction/TransactionService.java
@@ -1,7 +1,10 @@
 package br.com.lucasromagnoli.cashcontrol.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author github.com/lucasromagnoli
@@ -15,6 +18,11 @@ public class TransactionService {
     public Transaction save(Transaction transaction) {
         // TODO: 10/7/20 - Inserir validacã́o de negócios
         return transactionRepository.save(transaction);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Transaction> findAll(Pageable pageable) {
+        return transactionRepository.findAll(pageable);
     }
 
     public boolean existsWithIncome(Integer id) {
