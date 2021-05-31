24beee7021331bcbd31340b95aa6691500726d28
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2021-05-31 09:10:51 -0300

    Implementando a internacionalização

--- src/main/java/br/com/lucasromagnoli/cashcontrol/CashControlApplication.java
+++ src/main/java/br/com/lucasromagnoli/cashcontrol/CashControlApplication.java
@@ -2,6 +2,11 @@ package br.com.lucasromagnoli.cashcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class CashControlApplication {
@@ -10,4 +15,11 @@ public class CashControlApplication {
 		SpringApplication.run(CashControlApplication.class, args);
 	}
 
+	@Bean
+	public MessageSource messageSource() {
+		final var source = new ReloadableResourceBundleMessageSource();
+		source.setDefaultEncoding(StandardCharsets.UTF_8.name());
+		source.setBasename("classpath:i18n/cashcontrol-mensagens");
+		return source;
+	}
 }
